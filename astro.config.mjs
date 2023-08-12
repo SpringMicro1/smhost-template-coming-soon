79edaa31885149eb877d7e19b67922d131c6cd95
David Buckley <runalldayandnight@gmail.com>
2023-08-12 11:18:02 -0600

    simplify coming soon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,9 @@ import { defineConfig } from "astro/config";
 import { METADATA } from "./src/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import prefetch from "@astrojs/prefetch";
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: METADATA.SITE_URL,
-  integrations: [
-    tailwind({ applyBaseStyles: false }),
-    sitemap(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    prefetch(),
-    react(),
-  ],
+  integrations: [tailwind(), sitemap()],
 });
